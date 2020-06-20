bbeef0555abac166e24c841e010b3196ef4f8d07
MDomska <domskamarija@gmail.com>
2020-06-20 13:50:14 +0300

    add product

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="product-image" v-bind:style="{ 'background-image': 'url(' + image + ')' }"></div>
+    <div class="product-info">
+      <h2 class="product-title">{{ title }}</h2>
+      <p class="product-description">{{ description }}</p>
+      <span class="product-price">{{ price }}</span>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ["image", "title", "description", "price"]
+};
+</script>
---
+++ src/components/ProductsList.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="product-container">
+    <div class="product" v-for="item in products" :key="item.id">
+      <ProductItem
+        :image="item.image"
+        :title="item.title"
+        :description="item.description"
+        :price="item.price"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductItem from "./ProductItem";
+
+export default {
+  name: "ProductList",
+  components: {
+    ProductItem
+  },
+  data: function() {
+    return {
+      products: [
+        {
+          id: 1,
+          title: "Піца La П’єц",
+          description:
+            "соус томатний, шинка, баварські ковбаски, сир моцарела, ковбаса салямі, прошуто.",
+          price: "167 грн",
+          image:
+            "https://www.lapiec-pizza.com.ua/wp-content/uploads/2018/07/Flamingo-1.jpg"
+        },
+
+        {
+          id: 2,
+          title: "Капрічоза",
+          description: "соус томатний, сир моцарела, шинка, свіжі гриби.",
+          price: "151 грн",
+          image:
+            "https://www.lapiec-pizza.com.ua/wp-content/uploads/2018/08/Capricciosa.jpg"
+        },
+        {
+          id: 3,
+          title: "Цезаріо",
+          description:
+            "соус вершковий, помідори, хрусткий салат, пармезан, куряче мясо, сир моцарела, перепелині яйця.",
+          price: "192 грн",
+          image:
+            "https://www.lapiec-pizza.com.ua/wp-content/uploads/2018/08/Cezario.jpg"
+        },
+        {
+          id: 4,
+          title: "Кватро формаджі",
+          description:
+            "соус вершковий, пармезан, сир моцарела, сир рікотта, сир дор блю,  волоський горіх, груша.",
+          price: "182 грн",
+          image:
+            "https://www.lapiec-pizza.com.ua/wp-content/uploads/2018/07/kvatro.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="css">
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+}
+*,
+*:before,
+*:after {
+  -box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+.wpaper {
+  width: 80%;
+  margin: 0 auto;
+}
+.product-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+.product {
+  width: 50%;
+  padding: 10px;
+}
+.product-image {
+  min-height: 300px;
+  max-width: 100%;
+  background-size: cover;
+  background-position-y: center;
+}
+</style>
